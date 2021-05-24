568d6fde43feb56389cb452438f447bcf619acf0
danaratagayeva <danara.tagayeva@gmail.com>
2021-05-24 19:36:16 -0400

    project3 commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+
+export function App() {
+  const [counter, setCounter]=useState(8)
+
+  const increment=()=>{
+    setCounter(counter+1)
+
+  }
+  const decrement=()=>{
+    setCounter(counter-1)
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter:{counter}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
 
-export default App;
